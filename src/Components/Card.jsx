2ea2c523565806390e57837790412fdc84c55e13
LWhovian <laraabcdefernandes@gmail.com>
2023-06-29 15:49:55 -0300

    problemas resolvidos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -19,13 +19,13 @@ const Card = () => {
           alt="doctor placeholder"
         />
         <div className={`card-body ${styles.CardBody}`}>
+
           {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
 
-              a href={`/detail/${dentista.matricula}`}>
+              a href={`/detail/${dentista.matricula}`}
               <h5 className={`card-title ${styles.title}`}>{dentista.nome}</h5>
               <p className={`card-title ${styles.title}`}>@{dentista.usuario.username}</p>
-          </a>
         </div>
       </div>
     </>
