f4556530275857da6a33153aefda28542d4e1128
Arthur Campolina <arthurcampolina@hotmail.com>
2023-06-29 21:51:36 -0300

    feat: cria contextos (theme, dentists e auth) + cria rotas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -16,7 +16,7 @@ const Card = ({ dentist }) => {
         <div className={`card-body ${styles.CardBody}`}>
           {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
+          <a href={`/dentist/${dentist.matricula}`}>
             <h5 className={`card-title ${styles.title}`}>{dentist.nome} {dentist.sobrenome}</h5>
           </a>
         </div>
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,31 +1,16 @@
 import React from "react";
 import Card from "../Components/Card";
-import axios from "axios";
+import { DentisContext } from "../Contexts/DentistContext";
 
 const Home = () => {
-  const [dentists, setDentists] = React.useState([])
-  async function fetchDentists() {
-    try {
-      const response = await axios('https://dhodonto.ctdprojetointegrador.com/dentista')
-      if (!response) {
-        console.error("No dentists were found!", response)
-        return
-      }
-      setDentists(response.data)
-    } catch (error) {
-      console.error("Error to fecth dentists", error)
-    }
-  }
-  React.useEffect(() => {
-    fetchDentists()
-  }, []);
+  const dentists = React.useContext(DentisContext)
 
   return (
     <>
       <h1>Home</h1>
       {dentists.map((dentist) => (
-        <div className="card-grid container">
-          <Card key={dentist.matricula} dentist={dentist} />
+        <div className="card-grid container" key={dentist.matricula}>
+          <Card dentist={dentist} />
         </div>
       ))}
     </>
