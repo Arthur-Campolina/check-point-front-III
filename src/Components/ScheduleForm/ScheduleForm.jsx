327f5d7c6d0b4c71ed85721c0da53c9e99753c9c
Arthur Campolina <arthurcampolina@hotmail.com>
2023-07-01 23:46:58 -0300

    fix: reidireciona para login e remove token em caso de erro da api

--- src/Components/ScheduleForm/ScheduleForm.jsx
+++ src/Components/ScheduleForm/ScheduleForm.jsx
@@ -3,12 +3,14 @@ import styles from "./ScheduleForm.module.css";
 import { DentistContext } from "../../Contexts/DentistContext";
 import api from "../../services/api";
 import { useAuth } from "../../Contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const ScheduleForm = () => {
-  const { getUserTokenLocalStorage } = useAuth()
+  const { getUserTokenLocalStorage, removeUserTokenLocalStorage } = useAuth()
   const token = getUserTokenLocalStorage()
   const { dentists } = React.useContext(DentistContext)
   const [patients, setPatients] = React.useState([]);
+  const navigate = useNavigate()
 
   const fetchPacientes = async () => {
     const response = await api('getAllPacients', "/paciente")
@@ -66,6 +68,12 @@ const ScheduleForm = () => {
         Authorization: `Bearer ${token}`,
       },
     })
+    if (!response) {
+      removeUserTokenLocalStorage()
+      alert('Erro ao acessar api, faça login novamente')
+      navigate('/login')
+    }
+    if (response) alert('Consulta cadastrada com sucesso!')
     console.log("Consulta Response", response)
   };
 
